516f3252b70027ec13666320da14052e3384ff34
adrian92612 <adrian.villamin0612@gmail.com>
2024-07-30 14:49:42 +0800

    added new message form

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,29 @@
 import { Router } from "express";
 
+const messages = [
+  {
+    id: 1,
+    user: "Amber",
+    text: "Hello Chingu!",
+    dateAdded: new Date(),
+  },
+];
+
 export const router = Router();
 
-router.get("/", (req, res) => res.send("TEST!"));
+router.get("/", (req, res) => res.render("index", { title: "Mini Message Board", messages }));
+
+router.get("/new", (req, res) => res.render("newMessageForm", { title: "Send Message" }));
+
+router
+  .route("/new")
+  .get((req, res) => res.render("newMessageForm", { title: "Send Message" }))
+  .post((req, res) => {
+    messages.push({
+      id: messages.length + 1,
+      user: req.body.username,
+      text: req.body.text,
+      dateAdded: new Date(),
+    });
+    res.redirect("/");
+  });
