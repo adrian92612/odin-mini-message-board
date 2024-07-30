a4213f8d23e16986c429544fa3813dc996648b6f
adrian92612 <adrian.villamin0612@gmail.com>
2024-07-30 15:06:48 +0800

    added the message details page

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,9 @@ const messages = [
   },
 ];
 
-export const router = Router();
+const router = Router();
 
 router.get("/", (req, res) => res.render("index", { title: "Mini Message Board", messages }));
-
-router.get("/new", (req, res) => res.render("newMessageForm", { title: "Send Message" }));
 
 router
   .route("/new")
@@ -27,3 +25,15 @@ router
     });
     res.redirect("/");
   });
+
+router.get("/messages/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (id > messages.length) {
+    res.send("This message does not exists");
+    return;
+  }
+  const msg = messages.find((msg) => msg.id === id);
+  res.render("messageDetails", { title: "Message Details", msg });
+});
+
+export { router, messages };
